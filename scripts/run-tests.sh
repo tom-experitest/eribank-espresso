0393b9aee4c3517e37d2690608a3347470f5b08e
Tom <tom_p@experitest.com>
2017-08-21 16:44:12 +0300

    changed to khaled cloud

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,2 +1,1 @@
-
 curl -X POST http://192.168.2.27:80/api/v1/execution-plan/execute-test-plan -H 'authorization: Basic a2hhbGVkYTpFeHBlcml0ZXN0MjAxMg==' -H 'cache-control: no-cache' -H 'content-type: multipart/form-data; --form-string 'deviceQueries=@os='\''android'\''' -F testPlan=id:38
