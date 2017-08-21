760992f81e66d9f85b519e55cd1b8ff4d2a45c8f
Tom <tom_p@experitest.com>
2017-08-21 16:44:01 +0300

    changed to khaled cloud

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,1 +1,2 @@
-#!/bin/bashcurl -X POST http://192.168.2.135:80/api/v1/execution-plan/execute-test-plan -H 'authorization: Basic a2hhbGVkYTpFeHBlcml0ZXN0MjAxMg==' -H 'cache-control: no-cache' -H 'content-type: multipart/form-data; --form-string 'deviceQueries=@os='\''android'\''' -F testPlan=Default:androidNewSuite --form-string 'app=@/app/build/outputs/apk/app-debug.apk' --form-string 'testApp=@/app/build/outputs/apk/app-debug-androidTest.apk' > ./target/TEST.xml
+
+curl -X POST http://192.168.2.27:80/api/v1/execution-plan/execute-test-plan -H 'authorization: Basic a2hhbGVkYTpFeHBlcml0ZXN0MjAxMg==' -H 'cache-control: no-cache' -H 'content-type: multipart/form-data; --form-string 'deviceQueries=@os='\''android'\''' -F testPlan=id:38
